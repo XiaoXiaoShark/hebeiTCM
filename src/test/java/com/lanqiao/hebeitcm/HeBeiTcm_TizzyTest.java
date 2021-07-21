17ce0ada9efb48f5ab0ecc1115272a5e14db59b4
Tizzy11 <1063493784@qq.com>
2021-07-21 15:56:18 +0800

    tizzy

---
+++ src/test/java/com/lanqiao/hebeitcm/HeBeiTcm_TizzyTest.java
@@ -0,0 +1,10 @@
+package com.lanqiao.hebeitcm;
+
+import org.junit.jupiter.api.Test;
+
+public class HeBeiTcm_TizzyTest {
+    @Test
+    public void testTizzy1(){
+        System.out.println("tizzy1");
+    }
+}
