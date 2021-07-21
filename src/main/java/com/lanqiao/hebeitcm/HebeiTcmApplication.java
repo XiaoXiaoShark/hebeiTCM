a73c15c72d15012ff2b0971f2379438087bc5b81
expei <1647718715@qq.com>
2021-07-21 16:28:56 +0800

    创建文创-旅游分支

--- src/main/java/com/lanqiao/hebeitcm/HebeiTcmApplication.java
+++ src/main/java/com/lanqiao/hebeitcm/HebeiTcmApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class HebeiTcmApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(HebeiTcmApplication.class, args);
     }
