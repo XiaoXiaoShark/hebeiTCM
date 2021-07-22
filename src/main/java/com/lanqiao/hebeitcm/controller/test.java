3c7a36bd4037de6aff7ebccf7546dd01e1bd2194
xyg318 <2235427056@qq.com>
2021-07-22 17:32:59 +0800

    开发产业发展首页模块

--- src/main/java/com/lanqiao/hebeitcm/controller/test.java
+++ src/main/java/com/lanqiao/hebeitcm/controller/test.java
@@ -1,9 +1,18 @@
 package com.lanqiao.hebeitcm.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
 /**
  * @author 黄瑞颖
  * @create 2021-07-21 11:10
  */
+@Controller
 public class test {
     // 测试用，可以删掉
+    @GetMapping("/")
+    public String index(){
+        return "Industrial_development/Industrial_development";
+    }
+
 }
